75057c09e08f26aeedce45bb405cc6abdcdd251b
Benjamin Novak <benovak@Benjamins-MacBook-Air-2.local>
2019-04-05 14:40:41 -0400

    a

--- main.py
+++ main.py
@@ -14,34 +14,38 @@ PROPERTY_DATASET = 'Property_values.xlsx'
 CODE_TRANSLATOR = {'D': "Dangerous",
                    'C': 'Definitely Declining',
                    'B': 'Still Desireable',
-                   'A': 'Best'}
+                   'A': 'Best'
+                   }
 
-NEIGHBORHOODS = {'Allston': {'zipcode': ['02134'], 'population': 28821, 'code': 'B'},
-                'Back Bay': {'zipcode': ['02116'], 'population': 21844,'code': 'D'},
-                'Beaconhill': {'zipcode': ['02108'], 'population': 9943,'code': 'B'},
-                'Brighton': {'zipcode': ['02135'], 'population': 45977,'code': 'C'},
-                'Chinatown':{'zipcode': ['02111'], 'population': 7510,'code': 'D'},
-                'Dorchester':{'zipcode': ['02121', '02122', '02124', '02125'], 'population': 88333,'code': 'C'},
-                'East Boston': {'zipcode': ['02118'], 'population': 40508,'code': 'B'},
-                'Longwood': {'zipcode': ['02115'], 'population': 1754,'code': 'D'},
-                'Fenway': {'zipcode': ['02215'], 'population': 21174,'code': 'D'},
-                'Hyde Park': {'zipcode': ['02136'], 'population': 31845,'code': 'C'},
-                'Jamaica Plain': {'zipcode': ['02130'], 'population': 41262,'code': 'A'},
-                'Mattapan': {'zipcode': ['02126'], 'population': 34391,'code': 'C'},
-                'Mission Hill': {'zipcode': ['02120'], 'population': 13929,'code': 'D'},
-                'North End': {'zipcode': ['02113'], 'population': 10605,'code': 'D'},
-                'Roslindale':{'zipcode': ['02131'], 'population': 35945,'code': 'C'},
-                'Roxbury': {'zipcode': ['02119'], 'population': 63672,'code': 'D'},
-                'South Boston': {'zipcode': ['02127'], 'population': 35200,'code': 'C'},
-                'South End': {'zipcode': ['02118'], 'population': 33638,'code': 'D'},
-                'West End': {'zipcode': ['02114'], 'population': 5330,'code': 'D'},
-                'West Roxbury': {'zipcode': ['02132'], 'population': 30442,'code': 'C'}
-                }
+NEIGHBORHOODS = {
+    'Allston': {'zipcode': ['02134'], 'population': 28821, 'code': 'B'},
+    'Back Bay': {'zipcode': ['02116'], 'population': 21844,'code': 'D'},
+    'Beaconhill': {'zipcode': ['02108'], 'population': 9943,'code': 'B'},
+    'Brighton': {'zipcode': ['02135'], 'population': 45977,'code': 'C'},
+    'Chinatown':{'zipcode': ['02111'], 'population': 7510,'code': 'D'},
+    'Dorchester':{'zipcode': ['02121', '02122', '02124', '02125'],
+                  'population': 88333,'code': 'C'},
+    'East Boston': {'zipcode': ['02118'], 'population': 40508,'code': 'B'},
+    'Longwood': {'zipcode': ['02115'], 'population': 1754,'code': 'D'},
+    'Fenway': {'zipcode': ['02215'], 'population': 21174,'code': 'D'},
+    'Hyde Park': {'zipcode': ['02136'], 'population': 31845,'code': 'C'},
+    'Jamaica Plain': {'zipcode': ['02130'], 'population': 41262,'code': 'A'},
+    'Mattapan': {'zipcode': ['02126'], 'population': 34391,'code': 'C'},
+    'Mission Hill': {'zipcode': ['02120'], 'population': 13929,'code': 'D'},
+    'North End': {'zipcode': ['02113'], 'population': 10605,'code': 'D'},
+    'Roslindale':{'zipcode': ['02131'], 'population': 35945,'code': 'C'},
+    'Roxbury': {'zipcode': ['02119'], 'population': 63672,'code': 'D'},
+    'South Boston': {'zipcode': ['02127'], 'population': 35200,'code': 'C'},
+    'South End': {'zipcode': ['02118'], 'population': 33638,'code': 'D'},
+    'West End': {'zipcode': ['02114'], 'population': 5330,'code': 'D'},
+    'West Roxbury': {'zipcode': ['02132'], 'population': 30442,'code': 'C'}
+    }
 
 
 def get_neighborhood_by_zipcode(zipcode): 
     for neighborhood, value in NEIGHBORHOODS.items():
-        results = list(map(int, value['zipcode'])) # converts list of string to list of ints 
+        # converts list ofstring to list of ints 
+        results = list(map(int, value['zipcode'])) 
         if zipcode in results: # checks if zipcode is part of the neigborhood
             return neighborhood  
     return None
@@ -58,7 +62,8 @@ def get_avg_poverty_rate(df):
         # checks if we have seen this area before
         if area in area_poverty_number:
             curr_siz = area_poverty_number[area]['population']
-            area_poverty_number[area] = {'population': curr_siz + num_of_low_income_people}
+            area_poverty_number[area] = {'population':
+                                         curr_siz + num_of_low_income_people}
         else:
             #sets the poverty population for that area
             area_poverty_number[area] = {'population': num_of_low_income_people}
@@ -68,8 +73,10 @@ def get_avg_poverty_rate(df):
         neighborhood_pop = value['population']
         if neighborhood in area_poverty_number:
             pov_pop = area_poverty_number[neighborhood]['population']
-            area_poverty_number[neighborhood]['rate'] = (pov_pop / neighborhood_pop) * 100
-            area_poverty_number[neighborhood]['code'] = NEIGHBORHOODS[neighborhood]['code']
+            area_poverty_number[neighborhood]['rate'] =(pov_pop /
+                                                        neighborhood_pop) * 100
+            neighborhood_code = NEIGHBORHOODS[neighborhood]['code']
+            area_poverty_number[neighborhood]['code'] = neighborhood_code
 
 
             
@@ -94,12 +101,17 @@ def get_average_grad_rate(df):
             curr_avg = neighborhood_avg_grad_rate[neighborhood]['rate']
             curr_size = neighborhood_avg_grad_rate[neighborhood]['size']
             # calculates average graduation rate for the neighborhood 
-            neighborhood_avg_grad_rate[neighborhood] = {'rate': ((curr_avg * curr_size) + graduation_rate) / (curr_size + 1),
-                                                   'size': curr_size + 1, 'code': NEIGHBORHOODS[neighborhood]['code']}
+            average_value = {'rate': ((curr_avg * curr_size) + graduation_rate)
+                             / (curr_size + 1),
+                            'size': curr_size + 1,
+                            'code': NEIGHBORHOODS[neighborhood]['code']}
+            neighborhood_avg_grad_rate[neighborhood] = average_value
         else:
             # sets the graduation rate for that neighborhood 
             neighborhood_avg_grad_rate[neighborhood] = {
-                'rate': graduation_rate, 'size': 1, 'code': NEIGHBORHOODS[neighborhood]['code']}
+                'rate': graduation_rate,
+                'size': 1,
+                'code': NEIGHBORHOODS[neighborhood]['code']}
         
     return neighborhood_avg_grad_rate
 
@@ -121,18 +133,25 @@ def get_average_property_val(df):
             curr_avg = neighborhood_avg_property_val[neighborhood]['rate']
             curr_size = neighborhood_avg_property_val[neighborhood]['size']
             # calculates average property value for the neighborhood 
-            neighborhood_avg_property_val[neighborhood] = {'rate': ((curr_avg * curr_size) + property_value) / (curr_size + 1),
-                                                   'size': curr_size + 1, 'code': NEIGHBORHOODS[neighborhood]['code']}
+            average_value = {'rate': ((curr_avg * curr_size) + property_value)
+                             / (curr_size + 1),
+                            'size': curr_size + 1,
+                             'code': NEIGHBORHOODS[neighborhood]['code']}
+            neighborhood_avg_property_val[neighborhood] = average_value 
         else:
             # sets the property value for that neighborhood 
             neighborhood_avg_property_val[neighborhood] = {
-                'rate': property_value, 'size': 1, 'code': NEIGHBORHOODS[neighborhood]['code']}
+                'rate': property_value,
+                'size': 1,
+                'code': NEIGHBORHOODS[neighborhood]['code']}
         
     return neighborhood_avg_property_val
 
 def get_avg_value_for_each_code(data): 
-    avg_code_value = {'A' : {'avg' : 0, 'size': 0}, 'B' : {'avg' : 0, 'size': 0}, 
-                      'C' : {'avg' : 0, 'size': 0}, 'D' : {'avg' : 0, 'size': 0}}
+    avg_code_value = {'A' : {'avg' : 0, 'size': 0},
+                      'B' : {'avg' : 0, 'size': 0}, 
+                      'C' : {'avg' : 0, 'size': 0},
+                      'D' : {'avg' : 0, 'size': 0}}
     
     # iterates thorugh each neighborhood 
     for neighborhood, value in data.items(): 
@@ -140,14 +159,15 @@ def get_avg_value_for_each_code(data):
         rate = value['rate']
         curr_avg = avg_code_value[code]['avg'] 
         curr_size = avg_code_value[code]['size'] 
-        # calculates the current running average and then the new average 
-        avg_code_value[code]['avg'] =  ((curr_avg *  curr_size) + rate) / (curr_size + 1)
+        # calculates the current running average and then the new average
+        average_value = ((curr_avg *  curr_size) + rate) / (curr_size + 1)
+        avg_code_value[code]['avg'] =  average_value
         # increments the running size by 1 
         avg_code_value[code]['size'] =  avg_code_value[code]['size'] + 1 
     
     return avg_code_value
 
-def ANOVA( averages_by_neighborhood):
+def anova( averages_by_neighborhood):
     '''
     params: averages by neighborhood and by code as Dictionaries
     returns: 
@@ -254,7 +274,7 @@ def neighborhood_bar_chart(neighborhood_data, y_value_title, chart_title):
         neighborhoods.append(key)
         rates.append(neighborhood_data[key]["rate"])
 
-    #sorts the lists from largest to smallest for a more visually appealing graph
+    #sorts the lists from largest to smallest for a better graph
     rates, neighborhoods = sort_lists (rates,neighborhoods )     
 
     x_axis = np.arange(len(neighborhoods))
@@ -273,19 +293,21 @@ def sort_lists( rates, neighborhoods):
     Returns list of numbers, neighborhoods as a tupple of lists
     Does: Sorts the rates from largest to smallest to make a more visually
     appealing graph. Used only in neighborhood_bar_chart. We later realized it
-    could be done with a premade function but didn't want to delete the hard
-    work.
+    could be done with a premade function but didn't want to delete the work.
     '''
+    
     ordered = True
+    nb = neighborhoods
+
     while ordered:
         for i in range( len(rates) + 1 ):
             if i == 0:
                 next
             elif i == len(rates):
-                return rates, neighborhoods
+                return rates, nb
             elif rates[i] > rates[i-1]:
                 rates[i], rates[i-1] = rates[i-1], rates[i]
-                neighborhoods[i], neighborhoods[i-1] = neighborhoods[i-1], neighborhoods[i]
+                nb[i], nb[i-1] = nb[i-1], nb[i]
                 break
     return
 
@@ -296,51 +318,61 @@ def main():
     #Graduation Rates
     df = pd.read_excel(HIGHSCHOOL_DATASET, sheet_name='Public_Schools')
     neighborhood_avg_graduation_rate = get_average_grad_rate(df)
-    avg_graduation_for_code = get_avg_value_for_each_code(neighborhood_avg_graduation_rate)
+    average = get_avg_value_for_each_code(neighborhood_avg_graduation_rate)
+    avg_graduation_for_code = average
 
     #Poverty Rates
-    df2 = pd.read_excel(POC2_DATASET, sheet_name='Climate_Ready_Boston_Social_Vul')
+    df2 = pd.read_excel(POC2_DATASET,
+                        sheet_name='Climate_Ready_Boston_Social_Vul')
     neighborhood_avg_poverty_rate = get_avg_poverty_rate(df2)
-    avg_poverty_for_code = get_avg_value_for_each_code(neighborhood_avg_poverty_rate)
-
+    average = get_avg_value_for_each_code(neighborhood_avg_poverty_rate)
+    avg_poverty_for_code = average
+    
     #Property Values
     df = pd.read_excel(PROPERTY_DATASET, sheet_name='Sheet1')
     neighborhood_avg_property_value = get_average_property_val(df)    
-    avg_property_value_for_code = get_avg_value_for_each_code(neighborhood_avg_property_value)
+    average = get_avg_value_for_each_code(neighborhood_avg_property_value)
+    avg_property_value_for_code = average
     
     
     '''statistical analysis'''
     #Graduation Rates
     print("\n Results of ANOVA for graduation rates:\n")
-    ANOVA( neighborhood_avg_graduation_rate )
+    anova( neighborhood_avg_graduation_rate )
 
     #Poverty Rates
     print("\n Results of ANOVA for poverty rates: \n")
-    ANOVA( neighborhood_avg_poverty_rate )
+    anova( neighborhood_avg_poverty_rate )
 
     #Property Values
     print("\n Results of ANOVA for property values: \n")
-    ANOVA( neighborhood_avg_property_value)
+    anova( neighborhood_avg_property_value)
 
     
     '''visualizations'''
     #average graduation rate by code
-    averages_bar_chart(avg_graduation_for_code, "graduation rate",
+    averages_bar_chart(avg_graduation_for_code,
+                       "graduation rate",
                        "Graduation Rate by Code")
     #average poverty rate by code
-    averages_bar_chart(avg_poverty_for_code,"poverty rate",
+    averages_bar_chart(avg_poverty_for_code,
+                       "poverty rate",
                       "Poverty Rate by Code")
     #average property values by code
-    averages_bar_chart( avg_property_value_for_code,"property value",
-                       "Average Property Value by Code")
+    averages_bar_chart( avg_property_value_for_code,
+                        "property value",
+                        "Average Property Value by Code")
     #graduation rates by neighborhood
-    neighborhood_bar_chart( neighborhood_avg_graduation_rate, "Graduation Rate",
-                           'Graduation Rates by Neighborhood')
+    neighborhood_bar_chart( neighborhood_avg_graduation_rate,
+                            "Graduation Rate",
+                            'Graduation Rates by Neighborhood')
     #poverty Rates by Neighborhood
-    neighborhood_bar_chart( neighborhood_avg_poverty_rate, "Poverty Rate",
+    neighborhood_bar_chart( neighborhood_avg_poverty_rate,
+                            "Poverty Rate",
                             'Poverty Rates by Neighborhood')
     #Average Property Values by Neighborhood
-    neighborhood_bar_chart(neighborhood_avg_property_value, "Average Property",
+    neighborhood_bar_chart(neighborhood_avg_property_value,
+                           "Average Property",
                            'Average Property Values by Neighborhood')
     
 main()
